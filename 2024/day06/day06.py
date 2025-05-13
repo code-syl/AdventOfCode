68800bc986f617b24ca4c01585b56d39db506f11
code-syl <93523097+code-syl@users.noreply.github.com>
2025-05-13 14:47:04 +0200

    added day06 pt 2 in python

--- 2024/day06/day06.py
+++ 2024/day06/day06.py
@@ -25,7 +25,7 @@ def count(grid: list[list[int]], n: int) -> int:
                 count += 1
     return count
 
-def traverse(grid: list[list[int]], startX: int, startY: int) -> int:
+def traverse(grid: list[list[int]], startX: int, startY: int, part1: bool = True) -> tuple[int, set[tuple[int, int, int]]]:
     directions = [
         [0, -1], # North
         [1,  0], # East
@@ -34,14 +34,20 @@ def traverse(grid: list[list[int]], startX: int, startY: int) -> int:
     ] # % len(directions)
     x, y = startX, startY
     numberOfTurns = 0
+    turns = set()
 
     # once the guard leaves the grid, their patrol is complete
     while x >= 0 and x < len(grid[0]) and y >= 0 and y < len(grid):
         # move in the direction the guard is facing (% 4)
         direction = directions[numberOfTurns % len(directions)]
         nextX, nextY = x + direction[0], y + direction[1]
+
+        # for part 2, raise an exception when you are looping
+        # use a Set for performance (o(1) lookup)
         if nextX < 0 or nextX >= len(grid[0]) or nextY < 0 or nextY >= len(grid):
             break
+        if (x, y, numberOfTurns % len(directions)) in turns:
+            raise Exception("looping")
 
         nextSymbol = grid[nextY][nextX]
         # if there is an obstacle, turn clockwise once but don't move
@@ -49,11 +55,19 @@ def traverse(grid: list[list[int]], startX: int, startY: int) -> int:
             numberOfTurns += 1
             continue
 
+        turn = (x, y, numberOfTurns % len(directions))
+        turns.add(turn)
         grid[y][x] = 2
+
         x, y = nextX, nextY
+
     grid[y][x] = 2 # mark last step
 
-    return count(grid, 2)
+    if part1:
+        return count(grid, 2), turns
+
+    # do not count for the second part
+    return 0, set()
 
 # parse input
 map = []
@@ -68,17 +82,30 @@ with open("input.real", "r") as file:
 # count the distinct steps
 # keep moving and turning until you move out of bounds
 startX, startY = find(map, 9)
-solution = traverse(map, startX, startY)
-assert solution == 5551, "incorrect answer for part 1"
+solution, route = traverse(map, startX, startY)
 print(f"Part 1 solution: {solution}")
 
 # part 2
-# reset starting position
-# get all positions of the guard's original path
-# make copy of map
-# try and change one coordinate each time to an obstacle
-# traverse and check if it is a loop
-# (might want to make function of part1 and add looping check...
-# ...by keeping track of the already traversed coordinates + directions...
-# ...if the same coordinate + direction occurs, it is a loop, then raise Error)
-map[startY][startX] = 9
+#
+# get the route of part 1
+# for each coordinate that is not the start point, place a wall
+# check for an Exception
+#   if yes, then add only the coordinates to the wall list
+# count unique walls that caused a loop
+walls = set()
+for point in route:
+    if point == (startX, startY, 0):
+        continue
+    try:
+        # set one of the path's coords to a wall, then traverse
+        map[point[1]][point[0]] = 1
+        _, _ = traverse(map, startX, startY)
+    except Exception:
+        # if looping, add to the wall list
+        walls.add((point[0], point[1]))
+    finally:
+        # reset the wall to a normal coordinate
+        map[point[1]][point[0]] = 0
+
+solution = len(walls)
+print(f"Part 2 solution: {solution}")
