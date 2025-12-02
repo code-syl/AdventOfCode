e2a2d599efffeff5af1e3d4ece7b1850d23746ef
code-syl <93523097+code-syl@users.noreply.github.com>
2025-12-02 16:20:05 +0100

    add example d01

---
+++ d01/Program.cs
@@ -0,0 +1,34 @@
+﻿var lines = File.ReadAllLines("./example.txt");
+var commands = lines.Select(l =>
+{
+    var direction = l.Substring(0,1) == "L" ? Direction.Left : Direction.Right;
+    var steps = Convert.ToInt32(l.Substring(1));
+    return (Direction: direction, Steps: steps);
+});
+
+var start = 50;
+var max = 99;
+var current = start;
+var password = 0;
+
+foreach (var command in commands)   
+{
+    switch (command.Direction)
+    {
+        case Direction.Left: 
+                current = ((max + 1) + (current - command.Steps) % (max + 1)) % (max + 1); // cycle negatively (max 100)
+                break;
+        case Direction.Right:
+                current = (current + command.Steps) % (max + 1); // cycle positively (max 100)
+                break;
+    }
+
+    Console.WriteLine(current);
+}
+
+
+enum Direction
+{
+    Left,
+    Right
+};
