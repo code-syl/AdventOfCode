163212b7bb87ed7406ecc2323ffe50f228155560
code-syl <93523097+code-syl@users.noreply.github.com>
2025-12-24 14:49:11 +0100

    aoc 2025 d3 p1

---
+++ 2025/d03/Program.cs
@@ -0,0 +1,55 @@
+﻿var lines = File.ReadAllLines("input.real");
+
+var banks = lines.Select(l =>
+{
+    return l.ToCharArray().Select(c => c - '0');
+}).ToArray();
+var sum = 0;
+
+// Part 1
+
+foreach (var b in banks)
+{
+    var bank = b.ToArray();
+
+    // walk from left to right per array - 1 to get the highest digit
+    var current = 0;
+    var highest = 0;
+    var first = 0;
+    var second = 0;
+
+    for (var i = current; i < bank.Length - 1; i++)
+    {
+        if (bank[i] == 9)
+        {
+            highest = bank[i];
+            current = i + 1;
+            break;
+        }
+
+        if (bank[i] > highest) {
+            highest = bank[i];
+            current = i;
+        }
+    }
+
+    // then continue where left off till end or 9
+    if (highest != 9) current++;
+    first = highest;
+    highest = 0;
+    for (var i = current; i < bank.Length; i++)
+    {
+        if (bank[i] == 9)
+        {
+            highest = bank[i];
+            break;
+        }
+
+        if (bank[i] > highest) highest = bank[i];
+    }
+
+    second = highest;
+    sum += Convert.ToInt32($"{first}{second}");
+}
+
+Console.WriteLine($"Part 1: {sum}");
